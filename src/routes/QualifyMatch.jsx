4eb206c7b3b57f9b592dfd2a3328119830e0bf91
Diego <drbv27@gmail.com>
2023-03-07 13:30:25 -0500

    addin tournament selector

--- src/routes/QualifyMatch.jsx
+++ src/routes/QualifyMatch.jsx
@@ -11,6 +11,7 @@ const QualifyMatch = () => {
     const [challengeInfo,setChallengeInfo] = useState(null)
     const [challengeTeams,setChallengesTeams] = useState(null)
     const [pairMatches,setPairMatches] = useState(null)
+    const [tournament, setTournament] = useState('ligue')
 
     const {eventId,matchId} = useParams()
     
@@ -20,7 +21,7 @@ const QualifyMatch = () => {
     useEffect(()=>{
         const fetchTeams = async () => {
             try{
-                const docRef = doc(firestore,'qualify',`${eventId}${matchId}`)
+                const docRef = doc(firestore,'eventchallenge',`${eventId}${matchId}`)
                 const docSnap = await getDoc(docRef)
     
                 if (docSnap.exists()) {
@@ -57,9 +58,9 @@ const QualifyMatch = () => {
 
 /*     console.log(challengeInfo)
     console.log(challengeTeams) */
-    if(pairMatches){
+/*     if(pairMatches){
         pairMatches.map((pair)=>console.log(pair[0],"vs",pair[1]))
-    }
+    } */
     
     const pairMatch = (list)=> {
         let pairs = new Array((list.length * (list.length - 1)) / 2),
@@ -72,12 +73,30 @@ const QualifyMatch = () => {
         return pairs;
     }
 
+    //console.log(challengeInfo)
+
 /*    if(challengeTeams){
        const parejas=  pairMatch(challengeTeams.teams)
        console.log(parejas)
        setPairMatches(parejas)
     }   */
 
+    if(challengeTeams){
+        console.log(challengeTeams)
+    }
+
+    const selTournament = (e)=>{
+        e.preventDefault()
+        
+        console.log('enviando',tournament)
+    }
+
+
+    const handleChange = (e)=>{
+        console.log(e.target.value)
+        setTournament(e.target.value)
+        
+    }
 
   return (
     <Layout>
@@ -86,8 +105,42 @@ const QualifyMatch = () => {
             <Link to={`/activeevents/${eventId}`}>atras</Link>
         </div>
         <div>
-            {challengeTeams ? <p>existen</p>:<p>no existen</p>}
+            {challengeTeams && <p>{challengeTeams.length} equipos inscritos</p>}
         </div>
+        <div>
+            <ul>
+                {challengeTeams && challengeTeams.map((team)=><li>{team.teamName}</li>)}
+            </ul>
+        </div>
+            <h3 className='text-center'>Selecciona el tipo de torneo</h3>
+            <form onSubmit={selTournament} className='text-center'>
+                <select
+                  className="mt-1
+                            ml-auto
+                            mr-auto
+                            form-input
+                            block
+                            w-1/2
+                            rounded-md
+                            border-gray-300
+                            shadow-sm
+                            focus:border-indigo-300 
+                            focus:ring 
+                            focus:ring-indigo-200 
+                            focus:ring-opacity-50"
+                  name="tournament"
+                  id="tournament"
+                  value={tournament}
+                  onChange={handleChange}
+                >
+                    <option value="ligue">liga</option>
+                    <option value="playoff">Liga + Playoffs</option>
+                    <option value="double">Liga Doble</option>
+                    <option value="elimination">Eliminación</option>
+                    <option value="elimligue">Eliminación + Liga</option>
+                </select>
+                <input type="submit" value='Sortear'/>
+            </form>
         <div>
             {pairMatches
             && <div>{pairMatches.map((pair)=>{
