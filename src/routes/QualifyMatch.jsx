bcb1425d89d96f68aaeb324a65d087e64ce89795
Diego <drbv27@gmail.com>
2023-03-09 10:01:48 -0500

    rendering the tournament

--- src/routes/QualifyMatch.jsx
+++ src/routes/QualifyMatch.jsx
@@ -16,9 +16,6 @@ const QualifyMatch = () => {
 
     const {eventId,matchId} = useParams()
     const navigate = useNavigate();
-    
-/*     console.log(eventId)
-    console.log(matchId) */
 
     useEffect(()=>{
         const fetchTeams = async () => {
@@ -27,7 +24,7 @@ const QualifyMatch = () => {
                 const docSnap = await getDoc(docRef)
     
                 if (docSnap.exists()) {
-                    console.log("Document data:", docSnap.data());
+                    /* console.log("Document data:", docSnap.data()); */
                     setChallengeInfo(docSnap.data())
                   } else {
                     // doc.data() will be undefined in this case
@@ -53,16 +50,10 @@ const QualifyMatch = () => {
 
     useEffect(()=>{
         if(challengeTeams){
-            const parejas = pairMatch(challengeTeams)
+            const parejas = shuffle(pairMatch(challengeTeams))
             setPairMatches(parejas)
           }
     },[challengeTeams])
-
-/*     console.log(challengeInfo)
-    console.log(challengeTeams) */
-/*     if(pairMatches){
-        pairMatches.map((pair)=>console.log(pair[0],"vs",pair[1]))
-    } */
     
     const pairMatch = (list)=> {
         let pairs = new Array((list.length * (list.length - 1)) / 2),
@@ -82,16 +73,14 @@ const QualifyMatch = () => {
         console.log(pairMatch(first))
         console.log(pairMatch(last))
     }
+
     function shuffle(array) {
         let m = array.length,
           t, i;
-      
         // While there remain elements to shuffle...
         while (m) {
-      
           // Pick a remaining element…
           i = Math.floor(Math.random() * m--);
-      
           // And swap it with the current element.
           t = array[m];
           array[m] = array[i];
@@ -100,36 +89,8 @@ const QualifyMatch = () => {
         return array;
       }
 
-    //console.log(challengeInfo)
-
-/*    if(challengeTeams){
-       const parejas=  pairMatch(challengeTeams.teams)
-       console.log(parejas)
-       setPairMatches(parejas)
-    }   */
-
-    if(challengeTeams){
-        console.log(challengeTeams)
-    }
-
     const selTournament = async(e)=>{
         e.preventDefault()
-/*         await setDoc(doc(firestore,"eventchallenge",`${id}${challenge.id}`),{
-            available:available,
-            eventId:id,
-            eventChallId:`${id}${challenge.id}`,
-            stage:stage,
-            type:challenge.challengeType,
-            innings:challenge.innings,
-            time:challenge.inningTime,
-            maxTeams:challenge.maxTeams,
-            teams:[],
-    
-          }) */
-        console.log('enviando');
-/*         const matches = shuffle(pairMatches); */
-        console.log(pairMatches)
-/*         console.log('partidos',matches) */
         setSeeMatches(true)
         await updateDoc(doc(firestore,"eventchallenge",`${eventId}${matchId}`),{
             stage:'scoring',
@@ -137,28 +98,7 @@ const QualifyMatch = () => {
             matches: pairMatches
         })
          navigate('/activeevents/:eventId/match/:matchId/tournament') 
-        {/* <Navigate to='/activeevents/:eventId/match/:matchId/games' replace={true}/> */}
     }
-
-    async function addEventData(e){
-        e.preventDefault();
-        /* console.log("probando",formData) */
-        await setDoc(doc(firestore,"events",id),formData)
-        challenges.forEach(async(challenge)=>{
-          if(challenge.challengeType==="match"){
-            await updateDoc(doc(firestore,"eventchallenge",`${eventId}${matchId}`),{
-                stage:'scoring',
-                tournament:'ligue',
-                matches:'prueba'
-      
-            })
-          }
-          
-    
-        })
-        cleanForm(e) 
-      }
-
 
     const handleChange = (e)=>{
         console.log(e.target.value)
@@ -169,76 +109,61 @@ const QualifyMatch = () => {
         doubleLigue(challengeTeams)
     }
 
-  return (
+    return (
     <Layout>
         <div>qualify</div>
-        <div>
-            <Link to={`/activeevents/${eventId}`}>atras</Link>
-        </div>
-        <div>
-            {challengeTeams && <p>{challengeTeams.length} equipos inscritos</p>}
-        </div>
-        <div>
-            <ul>
-                {challengeTeams && challengeTeams.map((team)=><li>{team.teamName}</li>)}
-            </ul>
-        </div>
-        {(challengeInfo && challengeInfo.stage==='scoring')
-        ?
-        <div>
-            <h3>El torneo está en curso</h3>
-            <Link to='/activeevents/:eventId/match/:matchId/tournament'>Ir al torneo</Link>
-        </div>
-        :
-        <div>
-            <h3 className='text-center'>Selecciona el tipo de torneo</h3>
-            <form onSubmit={selTournament} className='text-center'>
-                <select
-                className="mt-1
-                            ml-auto
-                            mr-auto
-                            form-input
-                            block
-                            w-1/2
-                            rounded-md
-                            border-gray-300
-                            shadow-sm
-                            focus:border-indigo-300 
-                            focus:ring 
-                            focus:ring-indigo-200 
-                            focus:ring-opacity-50"
-                name="tournament"
-                id="tournament"
-                value={tournament}
-                onChange={handleChange}
-                >
-                    <option value="ligue">Liga</option>
-                    <option value="playoff">Liga + Playoffs</option>
-                    {(challengeTeams && challengeTeams.length%2 ===0) && <option value="double">Liga Doble</option> }
-                    {(challengeTeams && challengeTeams.length%2 ===0) && <option value="elimligue">Eliminación + Liga</option> }
-                    {(challengeTeams && (Math.log(challengeTeams.length)/Math.log(2))%1 ===0) && <option value="elimination">Eliminación</option> }
-                </select>
-                {/*  */}
-                <input type="submit" value='Sortear'/>
-            </form>
-        </div>
-        }
-
-        <div>
-        {/* {(challengeInfo && challengeInfo.stage==='scoring')?'mensaje':'boton' } */}
-{/*             {(pairMatches && seeMatches)
-            && <div>
-                <h3>Partidos:</h3>
-                {shuffle(pairMatches).map((pair)=>{
-                return(<div className='flex mb-2'>
-                    <p>{`${pair[0].teamName} vs ${pair[1].teamName}`}</p>
-                    <Link to="/" className='bgsec text-white p-0.5 ml-1 rounded'>iniciar</Link>
-                    </div>)
-            })}</div>
-            } */}
-        </div>
+            <div>
+                <Link to={`/activeevents/${eventId}`}>atras</Link>
+            </div>
+            <div>
+                {challengeTeams && <p>{challengeTeams.length} equipos inscritos</p>}
+            </div>
+            <div>
+                <ul>
+                    {challengeTeams && challengeTeams.map((team)=><li key={team.id}>{team.teamName}</li>)}
+                </ul>
+            </div>
+            {(challengeInfo && challengeInfo.stage==='scoring')
+            ?
+            <div>
+                <h3>El torneo está en curso</h3>
+                <Link to={`/activeevents/${eventId}/match/${matchId}/tournament`}>Ir al torneo</Link>
+            </div>
+            :
+            <div>
+                <h3 className='text-center'>Selecciona el tipo de torneo</h3>
+                <form onSubmit={selTournament} className='text-center'>
+                    <select
+                    className="mt-1
+                                ml-auto
+                                mr-auto
+                                form-input
+                                block
+                                w-1/2
+                                rounded-md
+                                border-gray-300
+                                shadow-sm
+                                focus:border-indigo-300 
+                                focus:ring 
+                                focus:ring-indigo-200 
+                                focus:ring-opacity-50"
+                    name="tournament"
+                    id="tournament"
+                    value={tournament}
+                    onChange={handleChange}
+                    >
+                        <option value="ligue">Liga</option>
+                        <option value="playoff">Liga + Playoffs</option>
+                        {(challengeTeams && challengeTeams.length%2 ===0) && <option value="double">Liga Doble</option> }
+                        {(challengeTeams && challengeTeams.length%2 ===0) && <option value="elimligue">Eliminación + Liga</option> }
+                        {(challengeTeams && (Math.log(challengeTeams.length)/Math.log(2))%1 ===0) && <option value="elimination">Eliminación</option> }
+                    </select>
+                    <input type="submit" value='Sortear'/>
+                </form>
+            </div>
+            }
     </Layout>
-  )
+    )
 }
 
 export default QualifyMatch
--- src/routes/Tournament.jsx
+++ src/routes/Tournament.jsx
@@ -1,10 +1,63 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import { Link, useParams, Navigate, useNavigate } from 'react-router-dom'
 import Layout from '../components/Layout'
+import app from '../firebase';
+import { collection,getDocs,getFirestore,doc,setDoc,getDoc,updateDoc } from 'firebase/firestore'
+
+const firestore = getFirestore(app)
 
 const Tournament = () => {
+    const [challengeInfo,setChallengeInfo] = useState(null)
+    const {eventId,matchId} = useParams()
+
+    useEffect(()=>{
+        const fetchTeams = async () => {
+            try{
+                const docRef = doc(firestore,'eventchallenge',`${eventId}${matchId}`)
+                const docSnap = await getDoc(docRef)
+    
+                if (docSnap.exists()) {
+                    /* console.log("Document data:", docSnap.data()); */
+                    setChallengeInfo(docSnap.data())
+                  } else {
+                    // doc.data() will be undefined in this case
+                    console.log("No such document!");
+                    fetchTeams();//ojo con esto Diego
+                  }
+            }catch (error){
+                console.log(error)
+            }
+        }
+        fetchTeams()
+    },[])
+
+    {challengeInfo && console.log(challengeInfo)}
+
+
+
   return (
     <Layout>
-        TORNEO
+        <h2>Torneo...</h2>
+        {challengeInfo 
+        &&
+        <div>
+            {challengeInfo.matches.map((team)=>{
+                return <div key={`${team.teamA.id}${team.teamB.id}`}
+                            className='flex justify-between ml-8 mr-8 mb-2'>
+                            {team.teamA.teamName} vs {team.teamB.teamName} 
+                            <button className='
+                                                bgsec
+                                                text-white
+                                                rounded-md
+                                                px-1
+                                                ml-2
+                                                '>
+                                Jugar
+                            </button>
+                    </div>
+            })}
+        </div>
+        }
     </Layout>
   )
 }
