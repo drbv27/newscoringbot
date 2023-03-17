534f4d1755ca85e7b1169bb8d721677863c97dc0
Diego <drbv27@gmail.com>
2023-03-17 08:35:13 -0500

    implementing the points function

--- src/routes/Scores.jsx
+++ src/routes/Scores.jsx
@@ -27,14 +27,46 @@ useEffect(()=>{
                 scoresArray = [...scoresArray,doc.data()]
                 /* console.log(doc.id, " => ", doc.data()); */
                 setScores(scoresArray)
-                /* console.log(scores) */
             });
+            console.log(scores[0].matches.matches)
             
 /*             const docRef = doc(firestore,'events',`${eventId}`)
             const docSnap = await getDoc(docRef) */
     }
     fetchEventChall()
 },[])
+
+const puntos = function(partidos,equipo){
+    const puntos=partidos.filter((partido)=> partido.teamA.teamName===equipo || partido.teamB.teamName===equipo).map((partido)=>{
+        if(partido.teamA.teamName===equipo){
+            return partido.pointsA
+        }
+        return partido.pointsB
+    }).reduce((prev,next)=>prev+next)
+
+    const golesFavor=partidos.filter((partido)=> partido.teamA.teamName===equipo||partido.teamB.teamName===equipo).map((partido)=>{
+        if(partido.teamA.teamName===equipo){
+            return partido.goalsA
+        }
+        return partido.goalsB
+    }).reduce((prev,next)=>prev+next)
+
+    const golesContra=partidos.filter((partido)=> partido.teamA.teamName===equipo||partido.teamB.teamName===equipo).map((partido)=>{
+        if(partido.teamA.teamName===equipo){
+            return partido.goalsB
+        }
+        return partido.goalsA
+    }).reduce((prev,next)=>prev+next)
+
+    return {team:equipo,
+            points:puntos,
+            gf:golesFavor,
+            gc:golesContra,
+            gd:golesFavor-golesContra}
+}
+
+const Disney = puntos(scores[0].matches.matches,"Disney")
+console.log(Disney)
 
 
 
